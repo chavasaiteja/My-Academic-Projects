d1cbe3bbf66ba34e500ff43a5798b04c24d0e084
chavasaiteja <chavasaiteja12345@gmail.com>
2017-10-08 22:19:11 -0700

    Add files via upload

---
+++ Python-Custom Language Intrepreter/sai_lexer_driver.py
@@ -0,0 +1,14 @@
+import sys
+from sai_lexer import *
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    file = open(filename)
+    characters = file.read()
+    print characters
+    file.close()
+    tokens = imp_lex(characters)
+    print tokens
+    print '\n'
+    for token in tokens:
+        print token
